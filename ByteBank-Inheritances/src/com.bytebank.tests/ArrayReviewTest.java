4210edc9f6657b5d7b7d574c7965a955bdf0c9f2
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-08-12 21:03:20 -0600

    ArrayReviewTest pt1

---
+++ ByteBank-Inheritances/src/com.bytebank.tests/ArrayReviewTest.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayReviewTest {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        int number = 40;
+        Integer numberObject = new Integer(40);
+        List list = new ArrayList();
+        list.add(number);
+        list.add(numberObject);
+    }
+}
